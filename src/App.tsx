5ab03ce81c41a4ab9a8ff16f90a4ac2fc516e627
zhangyu1818 <hey@zhangyu.dev>
2024-11-22 20:50:00 +0800

    added collapsible settings panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import { Suspense, lazy } from 'react'
+import { Suspense, lazy, useState } from 'react'
 
-import { Content, Heading, IllustratedMessage } from '@adobe/react-spectrum'
+import {
+  Button,
+  Content,
+  Heading,
+  IllustratedMessage,
+  SearchField,
+} from '@adobe/react-spectrum'
 import Timeout from '@spectrum-icons/illustrations/Timeout'
+import ChevronUp from '@spectrum-icons/workflow/ChevronUp'
+import clsx from 'clsx'
+import { useFlip } from 'use-flip'
 
+import { FormField } from './components/form-field'
 import { PreviewDialog } from './components/preview-dialog'
 import { Card } from './components/styled'
 import { StylesForm } from './components/styles-form'
@@ -15,11 +25,38 @@ const VirtualGrid = lazy(() =>
 )
 
 function App() {
+  const [expanded, setExpanded] = useState(true)
+  const ref = useFlip([expanded], {
+    dimensions: 'height',
+    duration: 300,
+    easing: 'ease-in-out',
+  })
+
   return (
     <div className='flex h-screen w-screen flex-col gap-8 bg-neutral-100 p-6 dark:bg-neutral-900'>
-      <Card>
-        <StylesForm />
-      </Card>
+      <div className='relative after:absolute after:-bottom-6 after:right-1/2 after:size-12 after:translate-x-1/2 after:rounded-full after:bg-white after:content-["_"]  after:dark:bg-neutral-800'>
+        <Card ref={ref} className='overflow-hidden'>
+          {!expanded && (
+            <FormField name='searchText'>
+              <SearchField />
+            </FormField>
+          )}
+          {expanded && <StylesForm />}
+        </Card>
+        <Button
+          UNSAFE_className='absolute z-10 -bottom-4 right-1/2 translate-x-1/2'
+          variant='primary'
+          onPress={() => setExpanded(!expanded)}
+        >
+          <ChevronUp
+            UNSAFE_className={clsx(
+              'transition-transform duration-300',
+              !expanded && 'rotate-180',
+            )}
+          />
+        </Button>
+      </div>
+
       <Card className='flex-1 overflow-hidden p-0'>
         <Suspense
           fallback={
