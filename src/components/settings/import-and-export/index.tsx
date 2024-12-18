ac336a6b72caee6cb9dab95578cd20df0ec3162a
schlibra <schlibra@163.com>
2024-12-18 13:42:29 +0800

    Optimize export method, make code more concise

--- src/components/settings/import-and-export/index.tsx
+++ src/components/settings/import-and-export/index.tsx
@@ -7,12 +7,17 @@ import { Button } from '@/components/ui/button'
 import { Label } from '@/components/ui/label'
 import { SettingsContainer } from '@/components/ui/styled'
 import { Textarea } from '@/components/ui/textarea'
+import { createColor } from '@/lib/utils'
 import { useStore } from '@/store'
+
+import { defaultStyles } from '@/store/default-value'
+import { Styles } from "@/store/interface";
 
 export const ImportAndExportSettings = () => {
   const cls = 'col-span-2 grid grid-cols-subgrid items-center'
   const [style, setStyle] = useStore((store) => store.styles)
   const [showData, setShowData] = useState('')
+  const keys: string[] = Object.keys(defaultStyles)
   return (
     <SettingsContainer>
       <div className={cls}>
@@ -20,7 +25,16 @@ export const ImportAndExportSettings = () => {
         <Button
           size='icon'
           onClick={() => {
-            setShowData(JSON.stringify(style))
+            const data = []
+            for (let i = 0; i < keys.length; i++) {
+              data[i] =
+                i === 0 || i === 3 || i === 17 ?
+                  data[i] = (style[keys[i] as keyof Styles] as any[]).map((item: {value: never}) => item.value) :
+                  i === 11 || i === 13 || i === 15 || i === 23 ?
+                    data[i] = (style[keys[i] as keyof Styles] as any[]).map((item: { value: any }[]) => [item[0], item[1], item[2], item[3], item[4].value]) :
+                    style[keys[i] as keyof Styles]
+            }
+            setShowData(JSON.stringify(data))
           }}
         >
           <Share />
@@ -31,7 +45,17 @@ export const ImportAndExportSettings = () => {
         <Button
           size='icon'
           onClick={() => {
-            setStyle(JSON.parse(showData))
+            const data = {} as Record<string, any>
+            for (let i = 0; i < keys.length; i++) {
+              const item = JSON.parse(showData)[i]
+              data[keys[i]] =
+                i === 0 || i === 3 || i === 17 ?
+                  item.map((_item: string) => createColor(_item)) :
+                  i === 11 || i === 13 || i === 15 || i === 23?
+                    item.map((_item: any) => [_item[0], _item[1], _item[2], _item[3], createColor(_item[4])]) :
+                    item
+            }
+            setStyle(data as Styles)
           }}
         >
           <Import />
